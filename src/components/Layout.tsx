556f218f971dc3475d861b1d50c742a801b196d2
Yoseph <yosephgustavo@hotmail.com>
2023-02-15 10:14:42 -0300

    base layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router-dom"
+import { AiOutlineShoppingCart } from "react-icons/ai"
+
+const Layout = () => {
+  return (
+    <>
+      <nav className="absolute top-0 left-0 right-0 bg-gray-600 py-5 px-5 flex justify-between">
+        <Link to="/" className="text-white">
+          Acme
+        </Link>
+        <Link to="/" className="text-white text-2xl">
+          <AiOutlineShoppingCart />
+        </Link>
+      </nav>
+      <main className="mt-16">
+        <Outlet />
+      </main>
+    </>
+  )
+}
+
+export default Layout
